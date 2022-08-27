210d6cbdaa0ae9b0b138805093fe4a42fe47c060
Xinrong Liu <liuxinrong@gmail.com>
2022-08-27 15:15:24 -0700

    Remove monorepo mode web project

--- project_x/web/apps/webapp/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { WebappModule } from './webapp.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(WebappModule);
-  await app.listen(3000);
-}
-bootstrap();
--- project_x/web/apps/webapp/src/webapp.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class WebappService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
