158d8add82c8f6296c58234f7e4af52eefc629db
Xinrong Liu <liuxinrong@gmail.com>
2022-08-27 16:10:42 -0700

    Modify description of open API

--- project_x/web/src/main.ts
+++ project_x/web/src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Project X')
+    .setDescription('Description for project X')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Project X')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
