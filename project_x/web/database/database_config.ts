c1000ae6edc293bf72f443914814bb0b8d2cb455
Xinrong Liu <liuxinrong@gmail.com>
2022-09-05 15:01:21 -0700

    Move TypeOrm config into database_config.ts

---
+++ project_x/web/database/database_config.ts
@@ -0,0 +1,43 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  client,
+  client_auth_info,
+  client_bank_info,
+  client_web_hooks,
+  company,
+  company_auth_info,
+  employer,
+  company_plugin,
+  user,
+  user_auth_info,
+  user_payout,
+  pay_summary,
+  pay_item,
+  pay_activity,
+} from '../database/entities';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '123456',
+  database: 'projectx',
+  entities: [
+    client,
+    client_auth_info,
+    client_bank_info,
+    client_web_hooks,
+    company,
+    company_auth_info,
+    employer,
+    company_plugin,
+    user,
+    user_auth_info,
+    user_payout,
+    pay_summary,
+    pay_item,
+    pay_activity,
+  ],
+  synchronize: true,
+};
--- project_x/web/src/app.module.ts
+++ project_x/web/src/app.module.ts
@@ -3,20 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { client, client_auth_info, client_bank_info, client_web_hooks, company, company_auth_info, employer, company_plugin, user, user_auth_info, user_payout, pay_summary, pay_item, pay_activity } from '../database/entities';
+import { typeOrmConfig } from '../database/database_config'
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'projectx',
-      entities: [client, client_auth_info, client_bank_info, client_web_hooks, company, company_auth_info, employer, company_plugin, user, user_auth_info, user_payout, pay_summary, pay_item, pay_activity],
-      synchronize: true,
-    })
+    TypeOrmModule.forRoot(typeOrmConfig)
   ],
   controllers: [AppController],
   providers: [AppService],
