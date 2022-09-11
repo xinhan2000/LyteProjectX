f8b3453add03e7a3895b49474cf7c77733787b22
Xinrong Liu <liuxinrong@gmail.com>
2022-09-10 20:34:02 -0700

    Enable webpack HMR (Hot-Module Replacement)

--- project_x/web/src/main.ts
+++ project_x/web/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,5 +17,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
