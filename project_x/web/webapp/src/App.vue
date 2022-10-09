37b51a24841452db4a55665b1d974c964e6e7d5c
Xinrong Liu <liuxinrong@gmail.com>
2022-10-09 11:30:54 -0700

    Add element-plus node module for webapp client app

--- project_x/web/webapp/src/App.vue
+++ project_x/web/webapp/src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Select company name:</span>
+      </div>
+    </template>
+    <div  @click="onClick('youtube')" class="text item">Youtube</div>
+    <div  @click="onClick('patreon')" class="text item">Patreon</div>
+    <div @click="onClick('shopify')" class="text item">Shopify</div>
+  </el-card>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {  ElCard } from 'element-plus'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+     ElCard
+  },
+
+  methods: {
+    onClick(name) {
+      console.log('onClick ' + name)
+      if (name === 'youtube') {
+        location.href = "https://projectx.i234.me/oauth2/code?company=youtube";
+      } 
+    }
+  },
 }
 </script>
 
@@ -22,5 +39,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 480px;
 }
 </style>
