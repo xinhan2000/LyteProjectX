6e688f3beebd6571d11221d6231a25d972606d37
Xinrong Liu <liuxinrong@gmail.com>
2022-10-09 18:57:28 -0700

    Import PasswordView in Vue router

--- project_x/web/webapp/src/router/index.ts
+++ project_x/web/webapp/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import PasswordView from '../views/PasswordView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
